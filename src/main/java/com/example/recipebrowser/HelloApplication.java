33aebd89af26637f3104d92a286b216954a1cf8c
r2pen2 <joedobbelaar@gmail.com>
2022-04-02 00:11:16 -0400

    Fixed FXML not being tied to controller class

--- src/main/java/com/example/recipebrowser/HelloApplication.java
+++ src/main/java/com/example/recipebrowser/HelloApplication.java
@@ -15,7 +15,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        // Check this out!
     }
 
     public static void main(String[] args) {
